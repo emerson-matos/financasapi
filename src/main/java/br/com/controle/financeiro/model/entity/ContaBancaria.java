e80846f90ce11a18a72cb068ae905c2ac09b8eb8
Emerson Almeida Matos <emegson@live.com>
2020-02-29 04:08:12 -0300

    add serializable imp to ContaBancaria

--- src/main/java/br/com/controle/financeiro/model/entity/ContaBancaria.java
+++ src/main/java/br/com/controle/financeiro/model/entity/ContaBancaria.java
@@ -1,5 +1,7 @@
 package br.com.controle.financeiro.model.entity;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,7 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
 @Entity
-public class ContaBancaria {
+public class ContaBancaria implements Serializable {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
