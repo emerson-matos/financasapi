f3b2720fcd8871c33a4227f2cdc9fed60929bc17
Emerson Almeida Matos <emegson@live.com>
2020-03-02 01:02:31 -0300

    standardizing language

--- src/main/java/br/com/controle/financeiro/model/entity/Institution.java
+++ src/main/java/br/com/controle/financeiro/model/entity/Institution.java
@@ -1,45 +1,53 @@
 package br.com.controle.financeiro.model.entity;
 
 import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Client implements Serializable {
+public class Institution implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long clientId;
-
+	private Long institutionId;
 	private String name;
+	private String identifier;
 
-	public Client() {
+	public Institution() {
 		super();
 	}
 
-	public Client(final String name) {
+	public Institution(final String name) {
 		super();
 		this.name = name;
 	}
 
-	public Long getId() {
-		return this.clientId;
+	public Long getInstitutionId() {
+		return institutionId;
 	}
 
-	public void setId(final Long id) {
-		this.clientId = id;
+	public void setIdInstituicao(final Long institutionId) {
+		this.institutionId = institutionId;
 	}
 
 	public String getName() {
-		return this.name;
+		return name;
 	}
 
 	public void setName(final String name) {
 		this.name = name;
 	}
 
+	public String getIdentifier() {
+		return identifier;
+	}
+
+	public void setIdentifier(final String identifier) {
+		this.identifier = identifier;
+	}
 }
