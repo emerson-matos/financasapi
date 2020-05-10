afc9af06a2a37916c714d5678db8ccad35e006c6
Emerson Almeida Matos <emegson@live.com>
2020-05-10 17:32:43 -0300

    adding controller for singup

--- src/main/java/br/com/controle/financeiro/controller/api/SingUp.java
+++ src/main/java/br/com/controle/financeiro/controller/api/SingUp.java
@@ -1,5 +1,13 @@
 package br.com.controle.financeiro.controller.api;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,8 +19,30 @@ public class SingUp {
     private static final Logger LOG = LoggerFactory.getLogger(SingUp.class);
 
     @PostMapping("/signup")
-    public void signUp(@RequestHeader String token) {
-        LOG.info("SignUp request for ${}", token);
-        //TODO(register) 
+    public String signUp(@RequestHeader String token) throws FirebaseAuthException {
+        LOG.info("SignUp request for {}", token);
+        // Load the service account key JSON file
+        // FileInputStream serviceAccount = new
+        // FileInputStream("path/to/serviceAccountKey.json");
+        String uid = null;
+        // Authenticate a Google credential with the service account
+        GoogleCredential googleCred;
+        try {
+            // Add the required scopes to the Google credential
+            googleCred = GoogleCredential.getApplicationDefault();
+            // Use the Google credential to generate an access token
+            GoogleCredential scoped = googleCred
+                    .createScoped(Arrays.asList("https://www.googleapis.com/auth/firebase.database",
+                            "https://www.googleapis.com/auth/userinfo.email"));
+            scoped.refreshToken();
+            uid = scoped.getAccessToken();
+            LOG.info("TOKEN: {}", uid);
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        // .createCustomToken(uid));
+        // TODO(register)
+
+        return uid;
     }
 }
