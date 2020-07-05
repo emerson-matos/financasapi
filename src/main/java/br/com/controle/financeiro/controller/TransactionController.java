7fddc5bc5db80d043d03aa362b00b7991510c13d
Emerson Almeida Matos <emegson@live.com>
2020-07-05 20:33:46 -0300

    refactor(sonar): removing unused import

--- src/main/java/br/com/controle/financeiro/controller/TransactionController.java
+++ src/main/java/br/com/controle/financeiro/controller/TransactionController.java
@@ -14,7 +14,6 @@ import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
