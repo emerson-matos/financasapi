6ceb089410ad1ac5cc01b41878116c3e3dcd731e
Emerson Almeida Matos <emegson@live.com>
2020-07-20 02:03:28 -0300

    feat: request auth and singup feature
    Signed-off-by: Emerson Almeida Matos <emegson@live.com>

--- src/main/java/br/com/controle/financeiro/configuration/SecurityConfig.java
+++ src/main/java/br/com/controle/financeiro/configuration/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import br.com.controle.financeiro.configuration.auth.firebase.FirebaseAuthenticationProvider;
@@ -23,78 +24,85 @@ import br.com.controle.financeiro.service.impl.UserServiceImpl;
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
 
-	public static class Roles {
-		public static final String ANONYMOUS = "ANONYMOUS";
-		public static final String USER = "USER";
-		public static final String ADMIN = "ADMIN";
+    public static class Roles {
+        public static final String ANONYMOUS = "ANONYMOUS";
+        public static final String USER = "USER";
+        public static final String ADMIN = "ADMIN";
 
-		private static final String ROLE = "ROLE_";
-		public static final String ROLE_ANONYMOUS = ROLE + ANONYMOUS;
-		public static final String ROLE_USER = ROLE + USER;
-		public static final String ROLE_ADMIN = ROLE + ADMIN;
-	}
+        private static final String ROLE = "ROLE_";
+        public static final String ROLE_ANONYMOUS = ROLE + ANONYMOUS;
+        public static final String ROLE_USER = ROLE + USER;
+        public static final String ROLE_ADMIN = ROLE + ADMIN;
 
-	@Order(Ordered.HIGHEST_PRECEDENCE)
-	@Configuration
-	protected static class AuthenticationSecurity extends GlobalAuthenticationConfigurerAdapter {
+        private Roles() {
 
-		@Autowired
-		@Qualifier(value = UserServiceImpl.NAME)
-		private UserDetailsService userService;
+        }
+    }
 
-		@Value("${br.com.controle.financeiro.firebase.enabled}")
-		private Boolean firebaseEnabled;
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    @Configuration
+    protected static class AuthenticationSecurity extends GlobalAuthenticationConfigurerAdapter {
 
-		@Autowired
-		private FirebaseAuthenticationProvider firebaseProvider;
+        @Value("${br.com.controle.financeiro.firebase.enabled}")
+        private Boolean firebaseEnabled;
 
-		@Override
-		public void init(AuthenticationManagerBuilder auth) throws Exception {
-			auth.userDetailsService(userService);
-			if (Boolean.TRUE.equals(firebaseEnabled)) {
-				auth.authenticationProvider(firebaseProvider);
-			}
-		}
-	}
+        private final UserDetailsService userService;
 
-	@Configuration
-	protected static class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+        private final FirebaseAuthenticationProvider firebaseProvider;
 
-		private static final String[] ADMIN_ENDPOINT = { "/admin/**", "/health/**" };
-		private static final String[] OPEN_ENDPOINT = { "/open/**", "/signup/**" };
-		private static final String[] USER_ENDPOINT = { "/transaction/**", "/bankaccount/**", "/card/**", "/client/**",
-				"/institution/**" };
+        public AuthenticationSecurity(@Qualifier(value = UserServiceImpl.NAME) UserDetailsService userService, FirebaseAuthenticationProvider firebaseProvider) {
+            this.userService = userService;
+            this.firebaseProvider = firebaseProvider;
+        }
 
-		@Value("${br.com.controle.financeiro.firebase.enabled}")
-		private Boolean firebaseEnabled;
+        @Override
+        public void init(AuthenticationManagerBuilder auth) throws Exception {
+            auth.userDetailsService(userService).passwordEncoder(new BCryptPasswordEncoder());
+            if (Boolean.TRUE.equals(firebaseEnabled)) {
+                auth.authenticationProvider(firebaseProvider);
+            }
+        }
+    }
 
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			if (Boolean.TRUE.equals(firebaseEnabled)) {
-				http.addFilterBefore(tokenAuthorizationFilter(), BasicAuthenticationFilter.class).authorizeRequests()//
-						.antMatchers(OPEN_ENDPOINT).hasAnyRole(Roles.ANONYMOUS)//
-						.antMatchers(USER_ENDPOINT).hasRole(Roles.USER)
-						.antMatchers(ADMIN_ENDPOINT).hasAnyRole(Roles.ADMIN)//
-						.and().csrf().disable()//
-						.anonymous().authorities(Roles.ROLE_ANONYMOUS);//
-			} else {
-				http.httpBasic().and().authorizeRequests()//
+    @Configuration
+    protected static class ApplicationSecurity extends WebSecurityConfigurerAdapter {
 
-						// .antMatchers(OPEN_ENDPOINT).hasAnyRole(Roles.ANONYMOUS)//
-						// .antMatchers(USER_ENDPOINT).hasRole(Roles.USER)//
-						// .antMatchers(ADMIN_ENDPOINT).hasAnyRole(Roles.ADMIN)//
-						.antMatchers("/**").permitAll()//
-						.and().csrf().disable()//
-						.anonymous().authorities(Roles.ROLE_ANONYMOUS);//
-			}
-		}
+        private static final String[] ADMIN_ENDPOINT = {"/admin/**", "/health/**"};
+        private static final String[] OPEN_ENDPOINT = {"/", "/open/**"};
+        private static final String[] USER_ENDPOINT = {"/api/**"};
 
-		@Autowired(required = false)
-		private FirebaseService firebaseService;
+        @Value("${br.com.controle.financeiro.firebase.enabled}")
+        private Boolean firebaseEnabled;
 
-		private FirebaseFilter tokenAuthorizationFilter() {
-			return new FirebaseFilter(firebaseService);
-		}
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            if (Boolean.TRUE.equals(firebaseEnabled)) {
+//                http.authorizeRequests()//
+//                        .antMatchers(OPEN_ENDPOINT).permitAll()//
+//                        .antMatchers(HttpMethod.GET, USER_ENDPOINT).permitAll()//
+//                        .anyRequest().authenticated()//
+//                        .and().csrf().disable()//
+//                        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                http.addFilterBefore(tokenAuthorizationFilter(), BasicAuthenticationFilter.class).authorizeRequests()//
+                        .antMatchers(OPEN_ENDPOINT).hasAnyRole(Roles.ANONYMOUS)//
+                        .antMatchers(USER_ENDPOINT).hasRole(Roles.USER)
+                        .antMatchers(ADMIN_ENDPOINT).hasAnyRole(Roles.ADMIN)//
+                        .and().csrf().disable()//
+                        .anonymous().authorities(Roles.ROLE_ANONYMOUS);//
+            } else {
+                http.authorizeRequests()//
+                        .antMatchers(HttpMethod.GET, USER_ENDPOINT).permitAll()
+                        .anyRequest().authenticated()
+                        .and().formLogin();
+            }
+        }
 
-	}
+        @Autowired(required = false)
+        private FirebaseService firebaseService;
+
+        private FirebaseFilter tokenAuthorizationFilter() {
+            return new FirebaseFilter(firebaseService);
+        }
+
+    }
 }
--- src/main/java/br/com/controle/financeiro/controller/api/SingUp.java
+++ src/main/java/br/com/controle/financeiro/controller/api/SingUp.java
@@ -1,48 +1,40 @@
 package br.com.controle.financeiro.controller.api;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Arrays;
+import javax.transaction.Transactional;
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthException;
+import br.com.controle.financeiro.configuration.auth.firebase.FirebaseTokenHolder;
+import br.com.controle.financeiro.service.FirebaseService;
+import br.com.controle.financeiro.service.UserService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SingUp {
     private static final Logger LOG = LoggerFactory.getLogger(SingUp.class);
 
-    @PostMapping("/signup")
-    public String signUp(@RequestHeader String token) throws FirebaseAuthException {
-        LOG.info("SignUp request for {}", token);
-        // Load the service account key JSON file
-        // FileInputStream serviceAccount = new
-        // FileInputStream("path/to/serviceAccountKey.json");
-        String uid = null;
-        // Authenticate a Google credential with the service account
-        GoogleCredential googleCred;
-        try {
-            // Add the required scopes to the Google credential
-            googleCred = GoogleCredential.getApplicationDefault();
-            // Use the Google credential to generate an access token
-            GoogleCredential scoped = googleCred
-                    .createScoped(Arrays.asList("https://www.googleapis.com/auth/firebase.database",
-                            "https://www.googleapis.com/auth/userinfo.email"));
-            scoped.refreshToken();
-            uid = scoped.getAccessToken();
-            LOG.info("TOKEN: {}", uid);
-        } catch (IOException e1) {
-            e1.printStackTrace();
-        }
-        // .createCustomToken(uid));
-        // TODO(register)
+    @Autowired(required = false)
+    private FirebaseService firebaseService;
 
-        return uid;
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/open/signup")
+    @Transactional
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void signUp(@RequestHeader(name = "X-Authorization-Firebase") String token) {
+        LOG.info("SignUp request for {}", token);
+
+        if (token.isBlank()) {
+            throw new IllegalArgumentException("FirebaseTokenBlank");
+        }
+        FirebaseTokenHolder tokenHolder = firebaseService.parseToken(token);
+        userService.registerUser(new UserService.RegisterUserInit(tokenHolder.getUid(), tokenHolder.getEmail()));
     }
 }
