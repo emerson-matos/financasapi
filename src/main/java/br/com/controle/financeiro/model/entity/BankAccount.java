61fe9155673385ed08fdae8af8731e66521d502f
Emerson Almeida Matos <emegson@live.com>
2020-07-20 01:45:54 -0300

    refactor: organize column names and table names of entities
    Signed-off-by: Emerson Almeida Matos <emegson@live.com>

--- src/main/java/br/com/controle/financeiro/model/entity/BankAccount.java
+++ src/main/java/br/com/controle/financeiro/model/entity/BankAccount.java
@@ -3,6 +3,7 @@ package br.com.controle.financeiro.model.entity;
 import java.io.Serializable;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -11,91 +12,92 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-@Entity
+@Entity(name = "bank_account")
 public class BankAccount implements Serializable {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long accountId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_account")
+    private Long accountId;
 
-	private String agency;
-	private String number;
-	private String dac;
+    private String agency;
+    private String number;
+    private String dac;
 
-	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.DETACH)
-	@JoinColumn(name = "client_id")
-	private Client owner;
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.DETACH)
+    @JoinColumn(name = "id_client")
+    private Client owner;
 
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "institution_id")
-	private Institution institution;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_institution")
+    private Institution institution;
 
-	public BankAccount() {
-		super();
-	}
+    public BankAccount() {
+        super();
+    }
 
-	public BankAccount(final Client owner, final Institution institution, final String agency, final String number,
-			final String dac, final Long accountId) {
-		super();
-		this.owner = owner;
-		this.institution = institution;
-		this.agency = agency;
-		this.number = number;
-		this.accountId = accountId;
-		this.dac = dac;
-	}
+    public BankAccount(final Client owner, final Institution institution, final String agency, final String number,
+                       final String dac, final Long accountId) {
+        super();
+        this.owner = owner;
+        this.institution = institution;
+        this.agency = agency;
+        this.number = number;
+        this.accountId = accountId;
+        this.dac = dac;
+    }
 
-	public Client getOwner() {
-		return owner;
-	}
+    public Client getOwner() {
+        return owner;
+    }
 
-	public void setDono(final Client owner) {
-		this.owner = owner;
-	}
+    public void setDono(final Client owner) {
+        this.owner = owner;
+    }
 
-	public Institution getInstitution() {
-		return institution;
-	}
+    public Institution getInstitution() {
+        return institution;
+    }
 
-	public void setInstitution(final Institution institution) {
-		this.institution = institution;
-	}
+    public void setInstitution(final Institution institution) {
+        this.institution = institution;
+    }
 
-	public String getAgency() {
-		return agency;
-	}
+    public String getAgency() {
+        return agency;
+    }
 
-	public void setAgency(final String agency) {
-		this.agency = agency;
-	}
+    public void setAgency(final String agency) {
+        this.agency = agency;
+    }
 
-	public String getNumber() {
-		return number;
-	}
+    public String getNumber() {
+        return number;
+    }
 
-	public void setNumber(final String number) {
-		this.number = number;
-	}
+    public void setNumber(final String number) {
+        this.number = number;
+    }
 
-	public String getDac() {
-		return dac;
-	}
+    public String getDac() {
+        return dac;
+    }
 
-	public void setDac(final String dac) {
-		this.dac = dac;
-	}
+    public void setDac(final String dac) {
+        this.dac = dac;
+    }
 
-	public Long getId() {
-		return accountId;
-	}
+    public Long getId() {
+        return accountId;
+    }
 
-	public void setId(Long accountId) {
-		this.accountId = accountId;
-	}
+    public void setId(Long accountId) {
+        this.accountId = accountId;
+    }
 
-	public BankAccount withId(Long id) {
-		this.setId(id);
-		return this;
-	}
+    public BankAccount withId(Long id) {
+        this.setId(id);
+        return this;
+    }
 
 }
--- src/main/java/br/com/controle/financeiro/model/entity/Client.java
+++ src/main/java/br/com/controle/financeiro/model/entity/Client.java
@@ -1,53 +1,56 @@
 package br.com.controle.financeiro.model.entity;
 
 import java.io.Serializable;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity
+@Entity(name = "client")
 public class Client implements Serializable {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long clientId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_client")
+    private Long clientId;
 
-	private String name;
+    private String name;
 
-	public Client() {
-		super();
-	}
+    public Client() {
+        super();
+    }
 
-	public Client(final String name) {
-		super();
-		this.name = name;
-	}
+    public Client(final String name) {
+        super();
+        this.name = name;
+    }
 
-	public Client(String name, Long id) {
-		this.name = name;
-		this.clientId = id;
-	}
+    public Client(String name, Long id) {
+        this.name = name;
+        this.clientId = id;
+    }
 
-	public Long getId() {
-		return this.clientId;
-	}
+    public Long getId() {
+        return this.clientId;
+    }
 
-	public void setId(final Long id) {
-		this.clientId = id;
-	}
+    public void setId(final Long id) {
+        this.clientId = id;
+    }
 
-	public String getName() {
-		return this.name;
-	}
+    public String getName() {
+        return this.name;
+    }
 
-	public void setName(final String name) {
-		this.name = name;
-	}
+    public void setName(final String name) {
+        this.name = name;
+    }
 
-	public Client withId(Long id) {
-		this.setId(id);
-		return this;
-	}
+    public Client withId(Long id) {
+        this.setId(id);
+        return this;
+    }
 
 }
