09238151ad956b0f1d99bdc0bddbc07aa185da8b
Emerson Almeida Matos <emegson@live.com>
2020-07-26 01:05:20 -0300

    style: indent using space

--- src/main/java/br/com/controle/financeiro/component/FirebaseParser.java
+++ src/main/java/br/com/controle/financeiro/component/FirebaseParser.java
@@ -7,16 +7,16 @@ import com.google.firebase.auth.FirebaseToken;
 import br.com.controle.financeiro.configuration.auth.firebase.FirebaseTokenHolder;
 
 public class FirebaseParser {
-	public FirebaseTokenHolder parseToken(String idToken) {
-		if (idToken.isBlank()) {
-			throw new IllegalArgumentException("FirebaseTokenBlank");
-		}
-		try {
-			FirebaseToken customToken = FirebaseAuth.getInstance().verifyIdToken(idToken);
+    public FirebaseTokenHolder parseToken(String idToken) {
+        if (idToken.isBlank()) {
+            throw new IllegalArgumentException("FirebaseTokenBlank");
+        }
+        try {
+            FirebaseToken customToken = FirebaseAuth.getInstance().verifyIdToken(idToken);
 
-			return new FirebaseTokenHolder(customToken);
-		} catch (Exception e) {
-			throw new FirebaseTokenInvalidException(e.getMessage());
-		}
-	}
+            return new FirebaseTokenHolder(customToken);
+        } catch (Exception e) {
+            throw new FirebaseTokenInvalidException(e.getMessage());
+        }
+    }
 }
