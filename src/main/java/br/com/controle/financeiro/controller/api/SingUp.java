5fdb2d54acaef4e5fe049854e32e82018e751c98
Emerson Almeida Matos <emegson@live.com>
2020-07-26 03:39:27 -0300

    fix: removing token debug logging

--- src/main/java/br/com/controle/financeiro/controller/api/SingUp.java
+++ src/main/java/br/com/controle/financeiro/controller/api/SingUp.java
@@ -5,9 +5,6 @@ import javax.transaction.Transactional;
 import br.com.controle.financeiro.configuration.auth.firebase.FirebaseTokenHolder;
 import br.com.controle.financeiro.service.FirebaseService;
 import br.com.controle.financeiro.service.UserService;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,8 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SingUp {
-    private static final Logger LOG = LoggerFactory.getLogger(SingUp.class);
-
     @Autowired(required = false)
     private FirebaseService firebaseService;
 
@@ -29,12 +24,11 @@ public class SingUp {
     @Transactional
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void signUp(@RequestHeader(name = "X-Firebase-User") String token) {
-        LOG.info("SignUp request for {}", token);
-
         if (token.isBlank()) {
             throw new IllegalArgumentException("FirebaseTokenBlank");
         }
         FirebaseTokenHolder tokenHolder = firebaseService.parseToken(token);
         userService.registerUser(new UserService.RegisterUserInit(tokenHolder.getUid(), tokenHolder.getEmail()));
     }
+
 }
