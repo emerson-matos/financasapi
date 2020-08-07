cc0080837d4a601294678f0a286ba523c2f86679
Emerson Almeida Matos <emegson@live.com>
2020-08-07 17:18:07 -0300

    ci: deploy.sh change script order

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,16 @@
 
 echo "INICIANDO DEPLOY.SH"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "MVN DEPLOY"
+mvn deploy -s .travis.settings.xml -DskipTests
 echo "DOCKER BUILD"
 docker build -t memerson .
 echo "DOCKER TAG LATEST"
-docker tag memerson $DOCKER_USERNAME/memerson:latest
-docker push $DOCKER_USERNAME/memerson:latest
+docker tag memerson "$DOCKER_USERNAME"/memerson:latest
+docker push "$DOCKER_USERNAME"/memerson:latest
 echo "DOCKER TAG POM_VERSION"
-docker tag memerson $DOCKER_USERNAME/memerson:$POM_VERSION
-docker push $DOCKER_USERNAME/memerson:$POM_VERSION
-echo "MVN DEPLOY"
-mvn deploy -s .travis.settings.xml -DskipTests
+docker tag memerson "$DOCKER_USERNAME"/memerson:"$POM_VERSION"
+docker push "$DOCKER_USERNAME"/memerson:"$POM_VERSION"
 
 echo "FIM DEPLOY.SH"
 exit 0
