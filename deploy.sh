5673cfa9429d35740768a7c250ba71d10597c66d
Emerson <emegson@live.com>
2020-08-07 16:46:34 -0300

    ci: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,11 @@ echo "INICIANDO DEPLOY.SH"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 echo "DOCKER BUILD"
 docker build -t memerson .
-echo "DOCKER TAG"
+echo "DOCKER TAG LATEST"
 docker tag memerson $DOCKER_USERNAME/memerson:latest
-docker tag memerson $DOCKER_USERNAME/memerson:$POM_VERSION
-echo "DOCKER PUSH"
 docker push $DOCKER_USERNAME/memerson:latest
+echo "DOCKER TAG POM_VERSION"
+docker tag memerson $DOCKER_USERNAME/memerson:$POM_VERSION
 docker push $DOCKER_USERNAME/memerson:$POM_VERSION
 echo "MVN DEPLOY"
 mvn deploy -s .travis.settings.xml -DskipTests
