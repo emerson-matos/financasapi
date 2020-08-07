a716317c78b64d80307255f22c33bb7faa3dd072
Emerson <emegson@live.com>
2020-08-07 16:47:11 -0300

    ci: snapshot.sh

--- snapshot.sh
+++ snapshot.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "INICIANDO SNAPSHOT.SH"
+
 if [[ "$POM_VERSION" =~ ^([0-9]+\.[0-9]+\.[0-9]+\-SNAPSHOT)$ ]]; then
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
   echo "DOCKER BUILD"
@@ -9,5 +10,6 @@ if [[ "$POM_VERSION" =~ ^([0-9]+\.[0-9]+\.[0-9]+\-SNAPSHOT)$ ]]; then
   echo "DOCKER PUSH"
   docker push $DOCKER_USERNAME/memerson:$SNAPSHOT_VERSION
 fi
+
 echo "FIM SNAPSHOT.SH"
 exit 0
